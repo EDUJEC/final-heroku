0b4fe1d963aae4fff652e7d0faec9d4d44fad65e
JOSEEDUARDO <JOSEEDUARDO@10.30.10.200>
2018-02-23 20:29:57 -0600

    clase 23/02/2018

--- src/main/java/org/unitec/finalheroku/ControladorMensajes.java
+++ src/main/java/org/unitec/finalheroku/ControladorMensajes.java
@@ -5,6 +5,9 @@
  */
 package org.unitec.finalheroku;
 
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ControladorMensajes {
+    @Autowired RepositorioMensajito repoMensa;
     
     @RequestMapping(value = "/mensajito",method = RequestMethod.GET,
             headers = {"Accept=application/json"})
-    public Mensajito hola(){
-        Mensajito mensa = new Mensajito ("Hola","Medamiedo los temblores");
-        return mensa;
+    public ArrayList<Mensajito> hola(){
+      
+        return(ArrayList<Mensajito>) repoMensa.findAll();
+        
+    }
+    
+    @RequestMapping(value="/mensajito/{id}/{titulo}/{cuerpo}",method=RequestMethod.GET,
+            headers={"Accept=application/json"})
+    
+    public Estatus guardar(@PathVariable String id,@PathVariable String titulo, @PathVariable String cuerpo){
+        repoMensa.save(new Mensajito(id,titulo,cuerpo));
+        return new Estatus(true,"Guardardo con exito");
+    }
+    
+    @RequestMapping(value="/mensajitos/{id}", method = RequestMethod.DELETE, headers = {"Accept=application/json"})
+    public Estatus borrarMensaje(@PathVariable String id){
+        
+        Estatus estatus= new Estatus(true , "borrado con exito");
+        repoMensa.delete(new Mensajito(id));
+        
+    return estatus;
     }
     
 }
--- src/main/java/org/unitec/finalheroku/Mensajito.java
+++ src/main/java/org/unitec/finalheroku/Mensajito.java
@@ -5,15 +5,60 @@
  */
 package org.unitec.finalheroku;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.springframework.data.annotation.Id;
+
 /**
  *
  * @author JOSEEDUARDO
  */
 public class Mensajito {
     
+    @Id
+    private String id;
+    private LocalDate fecha;
+
+    public Mensajito(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Mensajito(String id, LocalDate fecha, LocalTime hora, String Titulo, String Cuerpo) {
+        this.id = id;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.Titulo = Titulo;
+        this.Cuerpo = Cuerpo;
+    }
+    private LocalTime hora;
+
+    public Mensajito(String id, String Titulo, String Cuerpo) {
+        this.id = id;
+        this.Titulo = Titulo;
+        this.Cuerpo = Cuerpo;
+        fecha=LocalDate.now();
+        hora=LocalTime.now();
+    }
+
+    public Mensajito(String id) {
+        this.id = id;
+    }
     private String Titulo;
 
     public Mensajito(String Titulo, String Cuerpo) {
+        this.Titulo = Titulo;
+        this.Cuerpo = Cuerpo;
+        fecha=LocalDate.now();
+        hora=LocalTime.now();
+    }
+
+    public Mensajito(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Mensajito(String id, LocalDate fecha, String Titulo, String Cuerpo) {
+        this.id = id;
+        this.fecha = fecha;
         this.Titulo = Titulo;
         this.Cuerpo = Cuerpo;
     }
@@ -36,5 +81,29 @@ public class Mensajito {
 
     public void setCuerpo(String Cuerpo) {
         this.Cuerpo = Cuerpo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
     }
 }
